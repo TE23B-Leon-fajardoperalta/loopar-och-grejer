1b58a864a1595f56a09e0debd5b17812c6286be6
Leone <leon.fajardoperalta@elev.ga.ntig.se>
2024-11-12 13:23:12 +0100

    etyjryuk

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,123 @@
+﻿// if (3 <= 6)
+// {
+//     Console.WriteLine("Hello, World");
+// }
+
+
+// Console.ReadLine();
+
+// __________________________________________
+
+// string anvandarnamn = Console.ReadLine();
+
+// if (anvandarnamn == "noname")
+// {
+//     Console.WriteLine("Welcome!");
+// }
+
+// Console.ReadLine();
+
+// // __________________________________________
+
+//  Console.WriteLine("skriv användarnamn");
+//  string avm = Console.ReadLine();
+
+//  Console.WriteLine("skriv losen");
+//  string losen = Console.ReadLine();
+
+//  // OM avm == dog OCH losen == dog
+//  // grattis
+//  // annars nej, del
+
+// if (avm == "dog" && losen == "dog2")
+// {
+//     Console.WriteLine("grattis");
+// }
+// else 
+// {
+//     Console.WriteLine("fel");
+// }
+// Console.ReadLine();
+// ______________________________________________________________
+
+//  string ta = "Hello, World";
+
+
+
+// for (int i = 0; i < 32 ; i++)
+// {
+//     if (i <= 32)
+//     {
+//         Console.WriteLine(ta);
+
+//     }
+// }
+
+// Console.ReadLine();
+
+// ________________________________________________________________
+// Console.WriteLine("skriv kod");
+
+
+// string tu = Console.ReadLine();
+
+// while (tu != "forkett") 
+// {
+//  Console.WriteLine("fel");
+//  tu = Console.ReadLine();
+// }
+
+// Console.WriteLine("korrekt");
+// Console.ReadLine();
+// //__________________________________________________________________
+
+
+// for (int i = 0; i < 5; i++)
+// {
+//     Console.WriteLine("skriv nummer");
+
+//     string te = Console.ReadLine();
+//     int to = 0;
+
+
+//     bool tm = int.TryParse(te, out to);
+
+//     if (to >= 5)
+//     {
+//         Console.WriteLine("Högre än 5!");
+//     }
+
+// }
+// // _____________________________________________________________________
+
+// int thu = 0;
+
+
+// bool the = false;
+
+// while (the == false)
+// {
+//     Console.WriteLine("Skriv ett nummer:");
+
+//     string tha = Console.ReadLine();
+//     the = int.TryParse(tha, out thu);
+
+
+//     if (the == true)
+//     {
+//         Console.WriteLine("wow");
+//     }
+// }
+
+// Console.ReadLine();
+
+// //____________________________________________________________________________
+Console.WriteLine("gissa siffra från 1 till 10");
+int slu = Random.Shared.Next(1,10);
+string sla = Console.ReadLine
+
+if 
+
+
+
+
